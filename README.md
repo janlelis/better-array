3dfeaa664b576ac697685d3b696441d74d36f3bb
Jan Lelis <mail@janlelis.de>
2015-09-28 21:33:48 +0200

    no tdd aaah

---
+++ README.md
@@ -0,0 +1,30 @@
+# better-array for JavaScript [![[npm]](https://img.shields.io/npm/v/better-array.svg)](https://www.npmjs.com/package/better-array)
+
+Optionally improve arrays with some Ruby inspired extras.
+
+Or via npm:
+
+    $ npm install better-array
+
+## Usage
+
+```javascript
+var a = require('better-array')
+
+console.log(
+  a([1,2,3]).minus([2,3,4])
+); // => [1]
+```
+
+## Documentation
+
+Included:
+
+* `.minus`
+* `.plus`
+* `.and`
+
+
+## MIT License
+
+Copyright (C) 2015 Jan Lelis <http://janlelis.com>. Released under the MIT license.
---
+++ index.js
@@ -0,0 +1,41 @@
+function betterArray(array){ return Object.create(BetterArray).init(array); }
+
+BetterArray = {
+  init: function(array){
+    this.native = array;
+    return this;
+  },
+
+  minus: function(other){
+    // should this return uniq result?
+    return this.native.filter(function(e){
+      return other.indexOf(e) < 0;
+    });
+  },
+  plus: function(other){
+    // should this return uniq result?
+    return this.native.concat(other);
+  },
+  and: function(other){
+    return this.native.filter(function(e){
+      return other.indexOf(e) >= 0;
+    });
+  },
+  // or: function(other){
+  //   // TODO
+  // },
+
+  // non array operations?
+  // includes: function(element){
+  //   return this.native.indexOf(element) >= 0;
+  // },
+
+  // delegators?
+
+  // reduce: function(cur, acc){ return this.native.reduce(cur, acc); },
+  // reduceRight: function(cur, acc){ return this.native.reduceRight(cur, acc); },
+  // every: function(fn){ return this.native.every(fn); }
+  // ...
+}
+
+module.exports = betterArray
