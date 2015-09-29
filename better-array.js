b99da006decfae761d8cff3cdc5181c4e5b3bcb1
Jan Lelis <mail@janlelis.de>
2015-09-29 15:55:26 +0200

    add strict mode and export to browser global in non-commonjs

--- better-array.js
+++ better-array.js
@@ -1,41 +1,50 @@
-function betterArray(array){ return Object.create(BetterArray).init(array); }
+'use strict';
 
-BetterArray = {
-  init: function(array){
-    this.native = array;
-    return this;
-  },
+(function(global, code){
+  if(typeof exports === 'object'){
+    module.exports = code();
+  } else{
+    global.betterArray = code();
+  }}(this, function(){
 
-  minus: function(other){
-    // should this return uniq result?
-    return this.native.filter(function(e){
-      return other.indexOf(e) < 0;
-    });
-  },
-  plus: function(other){
-    // should this return uniq result?
-    return this.native.concat(other);
-  },
-  and: function(other){
-    return this.native.filter(function(e){
-      return other.indexOf(e) >= 0;
-    });
-  },
-  or: function(other){
-    // TODO
-  },
+    var BetterArray = {
+      init: function(array){
+        this.native = array;
+        return this;
+      },
 
-  // non array operations?
-  includes: function(element){
-    return this.native.indexOf(element) >= 0;
-  },
+      minus: function(other){
+        // should this return uniq result?
+        return this.native.filter(function(e){
+          return other.indexOf(e) < 0;
+        });
+      },
+      plus: function(other){
+        // should this return uniq result?
+        return this.native.concat(other);
+      },
+      and: function(other){
+        return this.native.filter(function(e){
+          return other.indexOf(e) >= 0;
+        });
+      },
+      or: function(other){
+        // TODO
+      },
 
-  // delegators?
+      // non array operations?
+      includes: function(element){
+        return this.native.indexOf(element) >= 0;
+      },
 
-  reduce: function(cur, acc){ return this.native.reduce(cur, acc); },
-  reduceRight: function(cur, acc){ return this.native.reduceRight(cur, acc); },
-  every: function(fn){ return this.native.every(fn); }
-  // ...
-}
+      // delegators?
 
-module.exports = betterArray
+      reduce: function(cur, acc){ return this.native.reduce(cur, acc); },
+      reduceRight: function(cur, acc){ return this.native.reduceRight(cur, acc); },
+      every: function(fn){ return this.native.every(fn); }
+      // ...
+    }
+
+    return function betterArray(array){ return Object.create(BetterArray).init(array); }
+  })
+);
