de81bd0c3b0f1c5db7c8aa3f0f2f351f70151469
Jan Lelis <mail@janlelis.de>
2015-10-01 17:00:07 +0200

    replace # with . in spec descriptions

--- spec/better_array_spec.js
+++ spec/better_array_spec.js
@@ -9,56 +9,56 @@ if (typeof(BetterArray) === 'undefined') {
 }
 
 describe('BetterArray', function() {
-  describe('#minus', function() {
+  describe('.minus', function() {
     it("takes values from array without the ones from the other array", function() {
       expect( ba([2, 3, 4]).minus([3, 4, 5]) ).toEqual([2]);
     });
   });
 
-  describe('#plus', function() {
+  describe('.plus', function() {
     it("concatenates two arrays", function() {
       expect( ba([2, 3, 4]).plus([3, 4, 5]) ).toEqual([2, 3, 4, 3, 4, 5]);
     });
   });
 
-  describe('#and', function() {
+  describe('.and', function() {
     it("returns the intersection", function() {
       expect( ba([2, 3, 4]).and([3, 4, 5]) ).toEqual([3, 4]);
     });
   });
 
-  describe('#or', function() {
+  describe('.or', function() {
     it("returns a uniqe array out of all elements of both arrays", function() {
       expect( ba([2, 3, 4]).or([3, 4, 4, 5]) ).toEqual([2, 3, 4, 5]);
     });
   });
 
-  describe('#unique', function() {
+  describe('.unique', function() {
     it("returns array with no double entries", function() {
       expect( ba([2, 3, 3, 4]).unique() ).toEqual([2, 3, 4]);
     });
   });
 
-  describe('#contains', function() {
+  describe('.contains', function() {
     it("returns array with no double entries", function() {
       expect( ba([2, 3, 4]).contains(4) ).toEqual(true);
       expect( ba([2, 3, 4]).contains(5) ).toEqual(false);
     });
   });
 
-  describe('#compact', function() {
+  describe('.compact', function() {
     it("returns array with all null and undefined values removed", function() {
       expect( ba([2, null, 3, undefined, 4, false]).compact() ).toEqual([2, 3, 4, false]);
     });
   });
 
-  describe('#times', function() {
+  describe('.times', function() {
     it("returns an array N times repeated", function() {
       expect( ba([2, 3, 4]).times(3) ).toEqual([2, 3, 4, 2, 3, 4, 2, 3, 4]);
     });
   });
 
-  describe('#count', function() {
+  describe('.count', function() {
     it("returns array size for no argument", function() {
       expect( ba([2, 3, 4]).count() ).toEqual(3);
     });
@@ -68,13 +68,13 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#size', function() {
+  describe('.size', function() {
     it("returns array size ", function() {
       expect( ba([2, 3, 4]).size() ).toEqual(3);
     });
   });
 
-  describe('#isEmpty', function() {
+  describe('.isEmpty', function() {
     it("returns false for non-empty arrays ", function() {
       expect( ba([2, 3, 4]).isEmpty() ).toEqual(false);
     });
@@ -84,13 +84,13 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#grep', function() {
+  describe('.grep', function() {
     it("returns an array of elements matching the (regex) test ", function() {
       expect( ba(["More", "Micro", "Modules"]).grep(/r/) ).toEqual(["More", "Micro"]);
     });
   });
 
-  describe('#zip', function() {
+  describe('.zip', function() {
     it("zips together multilpe arrays", function() {
       expect(
         ba([2, 3, 4]).zip([9, 8, 7], ["a", "b", "c"])
@@ -104,7 +104,7 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#clone', function() {
+  describe('.clone', function() {
     it("returns a shallow copy of the array", function() {
       var object = [2, 3, 4];
       expect( ba(object).clone() ).toEqual(object);
@@ -117,7 +117,7 @@ describe('BetterArray', function() {
   });
 
 
-  describe('#at', function() {
+  describe('.at', function() {
     it("returns value at that index for one argument", function() {
       expect( ba([2, 3, 4]).at(1) ).toEqual(3);
     });
@@ -127,13 +127,13 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#sliceLength', function() {
+  describe('.sliceLength', function() {
     it("returns a the slice, but 2nd params defines length of slice", function() {
       expect( ba([2, 3, 4]).sliceLength(1,2) ).toEqual([3, 4]);
     });
   });
 
-  describe('#first', function() {
+  describe('.first', function() {
     it("returns the first value for no argument", function() {
       expect( ba([2, 3, 4]).first() ).toEqual(2);
     });
@@ -143,7 +143,7 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#last', function() {
+  describe('.last', function() {
     it("returns the last value for no argument", function() {
       expect( ba([2, 3, 4]).last() ).toEqual(4);
     });
@@ -153,19 +153,19 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#take', function() {
+  describe('.take', function() {
     it("returns the first N values", function() {
       expect( ba([2, 3, 4]).take(2) ).toEqual([2, 3]);
     });
   });
 
-  describe('#drop', function() {
+  describe('.drop', function() {
     it("returns the all of the arrays values, except the first N", function() {
       expect( ba([2, 3, 4]).drop(2) ).toEqual([4]);
     });
   });
 
-  describe('#toArray', function() {
+  describe('.toArray', function() {
     it("returns native array", function() {
       var object = [2, 3, 4];
       expect( ba(object).toArray() ).toBe(object);
@@ -173,7 +173,7 @@ describe('BetterArray', function() {
   });
 
 
-  describe('#map', function() {
+  describe('.map', function() {
     it("delegates to native .map", function() {
       expect(
         ba([2, 3, 4]).map(function(e){ return e + 1; })
@@ -181,7 +181,7 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#each', function() {
+  describe('.each', function() {
     it("delegates to native .forEach", function() {
       var res = [];
       ba([2, 3, 4]).each(function(e, i){ res[i] = e + 1; })
@@ -189,7 +189,7 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#filter', function() {
+  describe('.filter', function() {
     it("delegates to native .filter", function() {
       expect(
         ba([2, 3, 4]).filter(function(e){ return e % 2 === 0; })
@@ -197,7 +197,7 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#reduce', function() {
+  describe('.reduce', function() {
     it("delegates to native .reduce", function() {
       expect(
         ba([2, 3, 4]).reduce(function(acc, cur){ return acc - cur; })
@@ -205,7 +205,7 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#reduceRight', function() {
+  describe('.reduceRight', function() {
     it("delegates to native .reduceRight", function() {
       expect(
         ba([2, 3, 4]).reduceRight(function(acc, cur){ return acc - cur; })
@@ -213,7 +213,7 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#reverse', function() {
+  describe('.reverse', function() {
     it("delegates to native .reverse", function() {
       expect( ba([2, 3, 4]).reverse() ).toEqual([4, 3, 2])
     });
@@ -225,7 +225,7 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#sort', function() {
+  describe('.sort', function() {
     it("delegates to native .sort", function() {
       expect( ba([4, 3, 2]).sort() ).toEqual([2, 3, 4])
     });
@@ -237,39 +237,39 @@ describe('BetterArray', function() {
     });
   });
 
-  describe('#slice', function() {
+  describe('.slice', function() {
     it("returns a the slice, 2nd params defines end index", function() {
       expect( ba([2, 3, 4]).slice(1,2) ).toEqual([3]);
     });
   });
 
-  describe('#indexOf', function() {
+  describe('.indexOf', function() {
     it("delegates to native .indexOf", function() {
       expect( ba([2, 3, 3, 4]).indexOf(3) ).toEqual(1);
     });
   });
 
-  describe('#lastIndexOf', function() {
+  describe('.lastIndexOf', function() {
     it("delegates to native .lastIndexOf", function() {
       expect( ba([2, 3, 3, 4]).indexOf(3) ).toEqual(1);
     });
   });
 
-  describe('#doesSome', function() {
+  describe('.doesSome', function() {
     it("delegates to native .some", function() {
       expect( ba([2, 3, 4]).doesSome(function(e){ return e % 5 == 0; }) ).toEqual(false);
       expect( ba([2, 3, 4]).doesSome(function(e){ return e % 4 == 0; }) ).toEqual(true);
     });
   });
 
-  describe('#doesEvery', function() {
+  describe('.doesEvery', function() {
     it("delegates to native .every", function() {
       expect( ba([2, 3, 4]).doesEvery(function(e){ return e % 2 == 0; }) ).toEqual(false);
       expect( ba([2, 3, 4]).doesEvery(function(e){ return e % 1 == 0; }) ).toEqual(true);
     });
   });
 
-  describe('#doesNone', function() {
+  describe('.doesNone', function() {
     it("returns true if none of the elements returns a true value for the test function", function() {
       expect( ba([2, 3, 4]).doesNone(function(e){ return e % 5 == 0; }) ).toEqual(true);
     });
